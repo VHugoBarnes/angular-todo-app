41cebfdd2a60e8c9dd80278a566b6f54aa0d0899
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-11 19:29:49 -0600

    Another way of controling forms

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -19,6 +20,12 @@ export class LabsComponent {
   name = signal("Nicole");
   disabled: boolean = true;
   image: string = "https://picsum.photos/123";
+
+  colorCtrl = new FormControl();
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe(console.log);
+  }
 
   handleChange() {
     console.log("HOLA");
