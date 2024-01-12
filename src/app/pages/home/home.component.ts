4ffe07081c2e54a394fe91497f3e3d0ca3209950
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-11 18:36:13 -0600

    Adding and deleting elements inside ngFor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,4 +15,15 @@ export class HomeComponent {
     "Create projects with Angular",
     "Integrate APIs with Angular"
   ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+    input.value = "";
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update(tasks => tasks.filter((_, i) => i !== index));
+  }
 }
